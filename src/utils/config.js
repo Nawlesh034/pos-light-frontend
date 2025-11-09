f7655729614b1b76a25af66dd92779c3100da999
Nawlesh034 <nn802301@gmail.com>
2025-11-09 12:03:57 +0530

    test-1

---
+++ src/utils/config.js
@@ -0,0 +1,7 @@
+// const BASE_URL = 'https://restorant-backend-new-veni-production.up.railway.app/api';
+
+// const BASE_URL = 'https://ssknf82q-6100.inc1.devtunnels.ms/api';
+
+const BASE_URL = 'http://localhost:6100/api';
+
+export const apiUrl = BASE_URL; 
