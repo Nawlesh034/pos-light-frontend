43649a544a33594ba83250432dd715d2ddb4b93d
Nawlesh034 <nn802301@gmail.com>
2025-11-09 12:37:59 +0530

    test-2

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,6 +2,6 @@
 
 // const BASE_URL = 'https://ssknf82q-6100.inc1.devtunnels.ms/api';
 
-const BASE_URL = 'http://localhost:6100/api';
+const BASE_URL = 'https://velvety-vacherin-cae6bc.netlify.app/api';
 
 export const apiUrl = BASE_URL; 
